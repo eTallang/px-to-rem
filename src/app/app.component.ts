2d2619e7a4111dc53d6e183d7aff520d948f7c9a
Erik Tallang <e.tallang@gmail.com>
2019-02-13 12:17:08 +0100

    feat(app): add radial gradient to input background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  pxValue: string;
+  pxValue = '0';
 
   get remValue(): number {
     if (this.pxValue) {
