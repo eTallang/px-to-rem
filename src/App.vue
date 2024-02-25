605e8454151a7a6e547498ca7b1af399e8cc0e24
Erik Tallang <e.tallang@gmail.com>
2024-02-25 15:30:59 +0100

    feat: create component for displaying rems

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,7 @@
   <sparks />
   <div class="page-content">
     <app-input v-model="pxValue" />
-    <button @click="copyRemValue()">{{ remValue }}</button>
-    <transition name="fade-in">
-      <div class="copied-text" v-if="showCopiedText">Copied!</div>
-    </transition>
+    <rem-button :value="remValue" />
   </div>
 </template>
 
@@ -13,20 +10,12 @@
 import { computed, ref } from "vue";
 import Sparks from "./components/Sparks.vue";
 import AppInput from "./components/Input.vue";
+import RemButton from "./components/RemButton.vue";
 import { useValueDrag } from "./hooks/useValueDrag";
 
 const pxValue = ref(16);
 const remValue = computed(() => `${pxValue.value / 16}rem`);
-const showCopiedText = ref(false);
 useValueDrag((delta) => (pxValue.value = pxValue.value + delta));
-
-const copyRemValue = () => {
-  navigator.clipboard.writeText(remValue.value).then(() => {
-    showCopiedText.value = true;
-
-    setTimeout(() => (showCopiedText.value = false), 800);
-  });
-};
 </script>
 
 <style scoped>
@@ -36,52 +25,5 @@ const copyRemValue = () => {
   flex-direction: column;
   gap: 6vh;
   align-items: center;
-}
-
-button {
-  position: relative;
-  font-size: max(5.5rem, 8vw);
-  background: transparent;
-  border: none;
-  color: var(--text-color);
-  cursor: pointer;
-  padding-inline: 32px;
-  user-select: none;
-
-  &::after {
-    content: "";
-    position: absolute;
-    inset: auto 0 0;
-    height: 4px;
-    background-color: var(--text-color);
-    scale: 0 1;
-    transition: scale 400ms ease;
-  }
-
-  &:focus-visible::after {
-    scale: 1 1;
-  }
-}
-
-.copied-text {
-  color: var(--text-color);
-  position: absolute;
-  letter-spacing: 2px;
-  bottom: 0;
-}
-
-.fade-in-enter-active,
-.fade-in-leave-active {
-  transition: all 0.5s ease;
-}
-
-.fade-in-enter-from {
-  translate: 0 -20px;
-  opacity: 0;
-}
-
-.fade-in-leave-to {
-  translate: 0 20px;
-  opacity: 0;
 }
 </style>
---
+++ src/components/RemButton.vue
@@ -0,0 +1,83 @@
+<template>
+  <button @click="copyRemValue()">
+    <transition name="fade-in">
+      <span :key="value">{{ value }}</span>
+    </transition>
+  </button>
+  <transition name="fade-in">
+    <div class="copied-text" v-if="showCopiedText">Copied!</div>
+  </transition>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const props = defineProps<{
+  value: string;
+}>();
+
+const showCopiedText = ref(false);
+
+const copyRemValue = () => {
+  navigator.clipboard.writeText(props.value).then(() => {
+    showCopiedText.value = true;
+
+    setTimeout(() => (showCopiedText.value = false), 800);
+  });
+};
+</script>
+
+<style scoped>
+button {
+  display: grid;
+  position: relative;
+  font-size: max(5.5rem, 8vw);
+  background: transparent;
+  border: none;
+  color: var(--text-color);
+  cursor: pointer;
+  padding-inline: 32px;
+  user-select: none;
+
+  &::after {
+    content: "";
+    position: absolute;
+    inset: auto 0 0;
+    height: 4px;
+    background-color: var(--text-color);
+    scale: 0 1;
+    transition: scale 400ms ease;
+  }
+
+  &:focus-visible::after {
+    scale: 1 1;
+  }
+
+  span {
+    grid-row: 1;
+    grid-column: 1;
+  }
+}
+
+.copied-text {
+  color: var(--text-color);
+  position: absolute;
+  letter-spacing: 2px;
+  bottom: 0;
+}
+
+.fade-in-enter-active,
+.fade-in-leave-active {
+  transition: all 150ms cubic-bezier(0.2, 0, 0.2, 1);
+}
+
+.fade-in-enter-from {
+  translate: 0 -20px;
+  opacity: 0;
+}
+
+.fade-in-leave-to {
+  translate: 0 20px;
+  opacity: 0;
+}
+</style>
